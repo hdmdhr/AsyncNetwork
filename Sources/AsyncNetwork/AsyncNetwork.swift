eafc239c0776d38bd32fb1a0926bab6f705f20bc
Daniel Hu <hdmdhr@gmail.com>
2022-09-26 10:46:51 -0600

    Add authorizer re-authorize logics.

--- Sources/AsyncNetwork/AsyncNetwork.swift
+++ Sources/AsyncNetwork/AsyncNetwork.swift
@@ -1,11 +1,24 @@
 import Foundation
 
 public protocol Authorizable {
+    var maxReAuthorizeTimes: Int { get }
+    
     @discardableResult
     func authorize(urlRequest: inout URLRequest) -> Bool
+    /// Optional. Only implement when you want to trigger re-authorize on failure.
+    func shouldReAuthorize(data: Data, urlResponse: URLResponse) -> Bool
+    /// Optional. Only implement when you need to support re-authorize on failure.
+    /// You should try to regain authorization in this method.
+    func refresh(with httpClient: HttpClientProtocol) async throws
 }
 
-
+public extension Authorizable {
+    var maxReAuthorizeTimes: Int { 1 }
+    
+    func shouldReAuthorize(data: Data, urlResponse: URLResponse) -> Bool { false }
+    
+    func refresh(with httpClient: HttpClientProtocol) async throws { }
+}
 
 public protocol HttpClientProtocol: AnyObject {
     /// - parameters:
@@ -60,7 +73,7 @@ public class HttpClient: HttpClientProtocol {
         
         // authorize
         if shouldAuthorize, let authorizer, !authorizer.authorize(urlRequest: &urlRequest) {
-            // failed authorizing request
+            throw NetworkError.cannotAuthorize(data: nil, urlResponse: nil)
         }
         
         // add body data
@@ -69,17 +82,43 @@ public class HttpClient: HttpClientProtocol {
         }
         
         do {
-            let response: Response
             let (data, urlResponse) = try await urlSession.data(for: urlRequest)
             
+            // execute request custom handler first (if there is one)
             if let customHandler {
-                response = try customHandler(data, urlResponse)
-            } else {   
-                response = try decoder.decode(Response.self, from: data)
+                return try customHandler(data, urlResponse)
             }
             
-            return response
+            // check if authorization failed (if there is an authorizer)
+            if let authorizer, authorizer.shouldReAuthorize(data: data, urlResponse: urlResponse) {
+                var retryTimes = 0
+                var _data: Data = data
+                var _urlResponse: URLResponse = urlResponse
+                
+                /// repeat for `maxReAuthorizeTimes`
+                /// 1. try to regain authorization
+                /// 2. authorize request
+                /// 3. fire request, try to decode response
+                /// before finally giving up and throw a `.cannotAuthorize` error
+                repeat {
+                    try await authorizer.refresh(with: self)
+                    guard authorizer.authorize(urlRequest: &urlRequest) else { break }
+                    (_data, _urlResponse) = try await urlSession.data(for: urlRequest)
+                    retryTimes += 1
+                    if let res = try? decoder.decode(Response.self, from: _data) {
+                        return res
+                    }
+                } while retryTimes < authorizer.maxReAuthorizeTimes
+                && authorizer.shouldReAuthorize(data: _data, urlResponse: _urlResponse)
+                
+                throw NetworkError.cannotAuthorize(data: _data, urlResponse: _urlResponse)
+            }
             
+            // decode data and return
+            return try decoder.decode(Response.self, from: data)
+            
+        } catch let decodingError as DecodingError {
+            throw NetworkError.decoding(decodingError)
         } catch {
             throw error
         }
--- Sources/AsyncNetwork/NetworkError.swift
+++ Sources/AsyncNetwork/NetworkError.swift
@@ -10,5 +10,7 @@ import Foundation
 public enum NetworkError: Error {
     /// Case wrapping Swift native `URLError`
     case urlError(URLError)
-    
+    /// Case wrapping Swift native `DecodingError`
+    case decoding(DecodingError)
+    case cannotAuthorize(data: Data?, urlResponse: URLResponse?)
 }
